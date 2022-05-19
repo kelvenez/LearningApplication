64f68f538dd650e8f7a6e0d742574a76d950bed7
kelvenez <kelvenea@gmail.com>
2022-05-19 15:36:18 +0800

    All done (Added new start page)

---
+++ app/src/main/java/com/example/studytoworld/StartActivity.java
@@ -0,0 +1,45 @@
+package com.example.studytoworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_start);
+        findViewById(R.id.loginStart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(StartActivity.this, Login.class);
+                StartActivity.this.startActivity(myIntent);
+            }
+        });
+        findViewById(R.id.regStart).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(StartActivity.this, Register.class);
+                StartActivity.this.startActivity(myIntent);
+            }
+        });
+        findViewById(R.id.loginStartbutton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(StartActivity.this, Login.class);
+                StartActivity.this.startActivity(myIntent);
+            }
+        });
+        findViewById(R.id.regStartbutton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(StartActivity.this, Register.class);
+                StartActivity.this.startActivity(myIntent);
+            }
+        });
+    }
+}
